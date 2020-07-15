7b73afe7ffb508101aa244d0db18311e40a048ef
vitalikspb88 <hotmail-88@mail.ru>
2020-07-15 12:20:15 +0300

    Custom TableViewCell

---
+++ MyPlaces/CustomTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlaces
+//
+//  Created by Macbook on 15.07.2020.
+//  Copyright © 2020 Macbook. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+}
