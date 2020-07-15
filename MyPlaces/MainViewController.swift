6d09ffa6630ae33bcf416e671183f4797b37252d
vitalikspb88 <hotmail-88@mail.ru>
2020-07-15 22:29:21 +0300

    searchBar is done

--- MyPlaces/MainViewController.swift
+++ MyPlaces/MainViewController.swift
@@ -10,15 +10,22 @@ import UIKit
 import RealmSwift
 
 class MainViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-
     
     @IBOutlet weak var segmentedControl: UISegmentedControl!
     @IBOutlet weak var reversedSortingButton: UIBarButtonItem!
     @IBOutlet weak var tableView: UITableView!
     
-    var places: Results<Place>!
-    var ascendingSorting = true
-    let searchController = UISearchController(searchResultsController: nil)
+    private var places: Results<Place>!
+    private var ascendingSorting = true
+    private let searchController = UISearchController(searchResultsController: nil)
+    private var filteredPlaces: Results<Place>!
+    private var searchBarIsEmpty: Bool {
+        guard let text = searchController.searchBar.text else { return false }
+        return text.isEmpty
+    }
+    private var isFiltering: Bool {
+        return searchController.isActive && !searchBarIsEmpty
+    }
     
     // MARK: - View Load
     
@@ -26,31 +33,47 @@ class MainViewController: UIViewController, UITableViewDelegate, UITableViewData
         super.viewDidLoad()
         
         places = realm.objects(Place.self)
+        
+        // setup the searh controller
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.placeholder = "Поиск"
+        navigationItem.searchController = searchController
+        definesPresentationContext = true
     }
-
+    
     // MARK: - Table view data source
-
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if isFiltering {
+            return filteredPlaces.count
+        }
         return places.isEmpty ? 0 : places.count
     }
-
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! CustomTableViewCell
         
-        let place = places[indexPath.row]
-
+        var place = Place()
+        
+        if isFiltering {
+            place = filteredPlaces[indexPath.row]
+        } else {
+            place = places[indexPath.row]
+        }
+        
         cell.nameLabel?.text = place.name
         cell.locationLabel.text = place.location
         cell.typeLabel.text = place.type
         cell.imageOfPlace.image = UIImage(data: place.imageData!)
-
+        
         cell.imageOfPlace?.layer.cornerRadius = cell.imageOfPlace.frame.size.height / 2
         cell.imageOfPlace?.clipsToBounds = true
-
+        
         return cell
     }
-
-    //MARK: TableView delegate
+    
+    //MARK: - TableView delegate
     
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
@@ -62,35 +85,39 @@ class MainViewController: UIViewController, UITableViewDelegate, UITableViewData
         
     }
     /*
-    override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
-
-        let place = places[indexPath.row]
-        let deleteAction = UIContextualAction(style: .destructive, title: "Delete") { (_, _, _) in
-            StorageManager.deleteObject(place)
-            tableView.deleteRows(at: [indexPath], with: .automatic)
-        }
-        return UISwipeActionsConfiguration(actions: [deleteAction])
-    }
- 
+     override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+     
+     let place = places[indexPath.row]
+     let deleteAction = UIContextualAction(style: .destructive, title: "Delete") { (_, _, _) in
+     StorageManager.deleteObject(place)
+     tableView.deleteRows(at: [indexPath], with: .automatic)
+     }
+     return UISwipeActionsConfiguration(actions: [deleteAction])
+     }
+     
+     
+     func tableView(_ tableView: UITableView, leadingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+     // swipe from left to right
+     return UISwipeActionsConfiguration(actions: [])
+     }
+     */
     
-    func tableView(_ tableView: UITableView, leadingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
-        // swipe from left to right
-        return UISwipeActionsConfiguration(actions: [])
-    }
-    */
-    
-//    MARK: Navigation
+    //    MARK: - Navigation
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         
         if segue.identifier == "ShowSegue" {
             guard let indexPath = tableView.indexPathForSelectedRow else { return }
-            let place = places[indexPath.row]
+            let place: Place
+            if isFiltering {
+                place = filteredPlaces[indexPath.row]
+            } else {
+                place = places[indexPath.row]
+            }
             let newPlaceVC = segue.destination as! NewPlaceViewController
             newPlaceVC.currentPlace = place
         }
     }
-
     
     @IBAction func unwindSegue(_ segue: UIStoryboardSegue) {
         
@@ -99,17 +126,17 @@ class MainViewController: UIViewController, UITableViewDelegate, UITableViewData
         tableView.reloadData()
     }
     
+//    MARK: - Sorting
+    
     @IBAction func sortSelection(_ sender: UISegmentedControl) {
-
+        
         sort()
     }
-    
     
     @IBAction func reversedSorting(_ sender: UIBarButtonItem) {
         
         ascendingSorting.toggle()
         sort()
-        
     }
     
     private func sort() {
@@ -123,6 +150,18 @@ class MainViewController: UIViewController, UITableViewDelegate, UITableViewData
     
 }
 
+//  MARK: - SearchBar
 
+extension MainViewController: UISearchResultsUpdating {
+
+    func updateSearchResults(for searchController: UISearchController) {
+        filterContentForSearchText(searchController.searchBar.text!)
+    }
+
+    private func filterContentForSearchText(_ searchText: String) {
+        filteredPlaces = places.filter("name CONTAINS[c] %@ OR location CONTAINS[c] %@", searchText, searchText)
+        tableView.reloadData()
+    }
+}
 
 
